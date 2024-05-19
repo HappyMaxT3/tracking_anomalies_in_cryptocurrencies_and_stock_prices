84005e6fc41e8589d8b66fb6cc08a6b6603460c5
Гудкова Ксения Дмитриевна <kdgudkova@edu.hse.ru>
2024-05-19 11:48:36 +0300

    Add code for checkbox on start page

--- main.py
+++ main.py
@@ -11,17 +11,22 @@ def index():
         portfel = request.form['portfel']
         period = request.form['period']
         algorithm.make_graph(stock, portfel, period)
+        if request.form.get('checkbox') == 'on':
+            if session.get('active') == 'active':
+                print("You will receive notice about notifications")
+            else:
+                return redirect(url_for("log_in_account"))
         return render_template('index.html', url='/static/images/plot.png')
     return render_template('index.html')
 
 @app.route("/login/", methods=['GET', 'POST'])
 def log_in_account():
-    if session.get('active') == '1':
+    if session.get('active') == 'active':
         return redirect(url_for("profile"))
     if request.method == 'POST':
         email_adress = request.form['email_adress']
         password = request.form['password']
-        session['active'] = '1'
+        session['active'] = 'active'
         return redirect(url_for("index"))
     return render_template('log_acc.html')
 
