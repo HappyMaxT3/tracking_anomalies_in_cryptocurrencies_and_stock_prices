9fccb1733377deaf7877bb8eb3b679d19c06b40b
Гудкова Ксения Дмитриевна <kdgudkova@edu.hse.ru>
2024-05-19 11:16:42 +0300

    Fix start page

--- main.py
+++ main.py
@@ -4,33 +4,31 @@ from src import algorithm
 app = Flask(__name__)
 app.secret_key = 'secret_key'
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        stock = request.form['stock']
+        portfel = request.form['portfel']
+        period = request.form['period']
+        algorithm.make_graph(stock, portfel, period)
+        return render_template('index.html', url='/static/images/plot.png')
     return render_template('index.html')
-
-@app.route("/chart/", methods=['POST'])
-def new_index():
-    stock = request.form['stock']
-    portfel = request.form['portfel']
-    period = request.form['period']
-    algorithm.make_graph(stock, portfel, period)
-    return render_template('index.html', url='/static/images/plot.png')
 
 @app.route("/login/", methods=['GET', 'POST'])
 def log_in_account():
-    if session.get('inactive') == False:
+    if session.get('active') == '1':
         return redirect(url_for("profile"))
     if request.method == 'POST':
         email_adress = request.form['email_adress']
         password = request.form['password']
-        session['inactive'] = False
+        session['active'] = '1'
         return redirect(url_for("index"))
     return render_template('log_acc.html')
 
 @app.route("/profile/", methods=['GET', 'POST'])
 def profile():
     if request.method == 'POST':
-        session.pop('inactive', None)
+        session.pop('active', None)
         return redirect(url_for("index"))
     return render_template("your_acc.html")
 
