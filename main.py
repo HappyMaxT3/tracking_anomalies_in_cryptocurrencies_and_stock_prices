3732e0571121a3b47490ad13f7c737548b8c7a41
Гудкова Ксения Дмитриевна <kdgudkova@edu.hse.ru>
2024-05-19 11:04:22 +0300

    Add new pages

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session
 from src import algorithm
 
 app = Flask(__name__)
+app.secret_key = 'secret_key'
 
 @app.route("/")
 def index():
@@ -15,9 +16,30 @@ def new_index():
     algorithm.make_graph(stock, portfel, period)
     return render_template('index.html', url='/static/images/plot.png')
 
-@app.route("/create_acc/")
-def create_account():
+@app.route("/login/", methods=['GET', 'POST'])
+def log_in_account():
+    if session.get('inactive') == False:
+        return redirect(url_for("profile"))
+    if request.method == 'POST':
+        email_adress = request.form['email_adress']
+        password = request.form['password']
+        session['inactive'] = False
+        return redirect(url_for("index"))
     return render_template('log_acc.html')
+
+@app.route("/profile/", methods=['GET', 'POST'])
+def profile():
+    if request.method == 'POST':
+        session.pop('inactive', None)
+        return redirect(url_for("index"))
+    return render_template("your_acc.html")
+
+@app.route("/create_account/", methods=['GET', 'POST'])
+def create_account():
+    if request.method == 'POST':
+        email_adress = request.form['email_adress']
+        return redirect(url_for("log_in_account"))
+    return render_template("create_acc.html")
 
 if __name__ == '__main__':
     app.run(debug = True)
