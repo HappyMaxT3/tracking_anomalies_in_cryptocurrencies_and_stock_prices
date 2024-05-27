9775e14a5c3cf7bee9121dc796c051e6c2d5d3c6
HappyMaxT3 <m9775531181@gmail.com>
2024-05-27 19:07:58 +0300

    add scroll button, new js, incorrect-massage

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,13 +1,23 @@
+document.getElementById('scrollButton').addEventListener('click', function() {
+    // Найти целевой элемент
+    var target = document.getElementById('targetSection');
+    
+    // Прокрутить до целевого элемента
+    target.scrollIntoView({ behavior: 'smooth' });
+});
+
+
+
 $(function() {
     // Инициализация Datepicker
-    $("#datepicker").datepicker({
+    $("#datepicker1").datepicker({
         dateFormat: "dd-mm-yy",
         onSelect: function(dateText) {
-            selectedDate = dateText;
+            selectedDateStart = dateText;
         }
     });
 
-    let selectedDate = "";
+    let selectedDateStart = "";
 
     // Показать календарь при нажатии на иконку
     // $("#calendar-icon").click(function() {
@@ -15,10 +25,27 @@ $(function() {
     // });
 
     // Сохранение даты в локальной переменной
-    $("#datepicker").on("change", function() {
-        selectedDate = $(this).val();
+    $("#datepicker1").on("change", function() {
+        selectedDateStart = $(this).val();
     });
 });
+
+$(function() {
+    // Инициализация Datepicker
+    $("#datepicker2").datepicker({
+        dateFormat: "dd-mm-yy",
+        onSelect: function(dateText) {
+            selectedDateEnd = dateText;
+        }
+    });
+
+    let selectedDateEnd = "";
+
+    $("#datepicker2").on("change", function() {
+        selectedDateEnd = $(this).val();
+    });
+});
+
 
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -45,35 +72,42 @@ document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('validNames');
     const stockInput = document.getElementById('stock');
     const portfelInput = document.getElementById('portfel');
-    const errorMessage = document.getElementById('errorMessage');
+    const stockErrorMessage = document.getElementById('stockErrorMessage');
+    const portfelErrorMessage = document.getElementById('portfelErrorMessage');
 
     const invalidChars = /[.,<>?=+\-~\[\]#*]/;
+    const validChasStock = /[ABCDEFGHIJKLMNOPQRSTUVWXYZ -]/;
 
     function validateInput() {
         const stockValue = stockInput.value;
         const portfelValue = portfelInput.value;
+        let isValid = true;
 
-        if (invalidChars.test(stockValue) || invalidChars.test(portfelValue)) {
-            errorMessage.textContent = 'Введенные данные некорректны';
-            errorMessage.style.display = 'block';
+        if (!validChasStock.test(stockValue)) {
+            stockErrorMessage.textContent = 'incorrect';
+            stockErrorMessage.parentElement.style.display = 'block';
+            isValid = false;
         } else {
-            errorMessage.style.display = 'none';
+            stockErrorMessage.parentElement.style.display = 'none';
         }
+
+        if (invalidChars.test(portfelValue)) {
+            portfelErrorMessage.textContent = 'incorrect';
+            portfelErrorMessage.parentElement.style.display = 'block';
+            isValid = false;
+        } else {
+            portfelErrorMessage.parentElement.style.display = 'none';
+        }
+
+        return isValid;
     }
 
     stockInput.addEventListener('input', validateInput);
     portfelInput.addEventListener('input', validateInput);
 
     form.addEventListener('submit', function(event) {
-        const stockValue = stockInput.value;
-        const portfelValue = portfelInput.value;
-
-        if (invalidChars.test(stockValue) || invalidChars.test(portfelValue)) {
-            errorMessage.textContent = 'Введенные данные некорректны';
-            errorMessage.style.display = 'block';
+        if (!validateInput()) {
             event.preventDefault();
-        } else {
-            errorMessage.style.display = 'none';
         }
     });
 });
